e36a270f6a2f5579743fbcf9110a2d6a9339c761
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-08-26 06:27:00 +0100

    changed home form

--- src/components/shared/Button/index.jsx
+++ src/components/shared/Button/index.jsx
@@ -1,5 +1,5 @@
 export default function Button({ text, ...rest }) {
     return (
-        <button {...rest} className="bg-secondary   w-full border-none  py-3 px-10 font-poppins cursor-pointer text-white font-semibold rounded-md ">{text}</button>
+        <button {...rest} className="bg-secondary hover:bg-green-700 duaration-500  w-full border-none  py-3 px-10 font-poppins cursor-pointer text-white font-semibold rounded-md ">{text}</button>
     )
 }
--- src/section/home/WhyChooseUs.jsx
+++ src/section/home/WhyChooseUs.jsx
@@ -1,52 +1,50 @@
 import Link from 'next/link'
-import React from 'react'
+import { useState } from 'react'
 import AboutCard from '../../components/AboutCard'
 import Contact from '../../components/Contact'
+import Button from '../../components/shared/Button'
+import RadioButton from '../../components/shared/Button/RadioButton'
+import { services } from '../../util/services'
 
 function WhyChooseUs() {
+  const [selected, setSelected] = useState(null);
+  const handleChange = (e) => setSelected(e.target.value);
+
   return (
     <div className="bg-[#e2e7f6]">
       <div className='max-w-[1250px] min-h-[600px] flex justify-center w-full h-full flex-col md:flex-row flex-col-reverse relative mx-auto'>
         <div className='basis[60%] px-4 md:pl-8 md:pr-0 py-12 w-full'>
-            <h1 className='font-semibold font-outfit leading-[1.5em] text-center md:text-left text-[1.8em] lg:text-[2.5em] text-text_primary'>Why Choose Us?</h1>
-            <p className='text-text_secondary font-poppins leading-[1.7rem] md:leading-[2rem]  text-center md:text-left  max-w-[680px] text-[1.1rem] md:text-[1.2rem]  mb-4 py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, pariatur culpa non molestiae ducimus deserunt explicabo, repudiandae ratione nostrum, numquam optio aliquam quia quidem atque?</p>
-            <Link href="/about" className='py-12'><span className="underline uppercase font-semibold font-outfit flex items-center justify-center md:justify-start text-primary hover:text-secondary ">Learn more about us &gt;&gt;</span></Link>
-            <div className='grid lg:grid-cols-2 py-8 gap-6 max-w-[700px]'>
-              <AboutCard img="ri-user-settings-fill" header="Experienced Staff" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />
-              <AboutCard img="ri-tools-fill" header="Best Equipment" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />           
-              <AboutCard img="ri-price-tag-2-fill" header="Reasonable Price" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />
-              <AboutCard img="ri-vip-crown-fill" header="Certified Company" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />           
-             </div>
+          <h1 className='font-semibold font-outfit leading-[1.5em] text-center md:text-left text-[1.8em] lg:text-[2.5em] text-text_primary'>Why Choose Us?</h1>
+          <p className='text-text_secondary font-poppins leading-[1.7rem] md:leading-[2rem]  text-center md:text-left  max-w-[680px] text-[1.1rem] md:text-[1.2rem]  mb-4 py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, pariatur culpa non molestiae ducimus deserunt explicabo, repudiandae ratione nostrum, numquam optio aliquam quia quidem atque?</p>
+          <Link href="/about" className='py-12'><span className="underline uppercase font-semibold font-outfit flex items-center justify-center md:justify-start text-primary hover:text-secondary ">Learn more about us &gt;&gt;</span></Link>
+          <div className='grid lg:grid-cols-2 py-8 gap-6 max-w-[700px]'>
+            <AboutCard img="ri-user-settings-fill" header="Experienced Staff" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />
+            <AboutCard img="ri-tools-fill" header="Best Equipment" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />
+            <AboutCard img="ri-price-tag-2-fill" header="Reasonable Price" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />
+            <AboutCard img="ri-vip-crown-fill" header="Certified Company" content="Morbi leo risus, porta ac consectetur acit vestibulum at eros. Integer posuere erat a ante venenatis dapibus." />
+          </div>
         </div>
-        <div className='h-[850px] lg:h-[800px] -mt-[60px] lg:-mt-[250px] rounded-xl mx-[1rem] lg:min-w-[420px] flex flex-col py-8 md:py-12 px-4 items-start shadow-lg border-lg bg-white'>
-            <h2 className="text-text_primary font-outfit font-semibold text-center flex items-center justify-center w-full text-[1.4em]">Request a Free Quote </h2>
-            <form action="" className='py-6  w-full px-2'>
-              <div className='flex mb-6  flex-col w-full'>
-                <label className="mb-2">Name</label>
-                <input type="text" className='w-full py-3 px-2 bg-input outline-none' />
-              </div>
-              <div className='flex mb-6 flex-col w-full'>
-                <label className="mb-2">Phone Number</label>
-                <input type="text" className='w-full py-3 px-2 bg-input outline-none' />
-              </div>
-              <div className='flex mb-6 flex-col w-full'>
-                <label className="mb-2">Select Date</label>
-                <input type="date" className='w-full py-3 px-2 bg-input outline-none' />
-              </div>
-              <div className='flex mb-6 flex-col w-full'>
-                <label className="mb-2">Choose a Service</label>
-                <input type="text" className='w-full py-3 px-2 bg-input outline-none' />
-              </div>
-              <div className='flex flex-col mb-6 w-full'>
-                <label className="mb-2">Zip Code</label>
-                <input type="text" className='w-full py-3 px-2 bg-input outline-none' />
-              </div>
-              <button className="bg-secondary  border-none flex items-center py-2 md:py-3 px-10 font-outfit w-full cursor-pointer text-white font-semibold rounded-md justify-center">Book Now</button>
-              <div className="flex flex-col md:flex-row md:items-center space-y-4 justify-between mt-5">
-                  <Contact icon="ri-phone-line" header="Hotline" content="+11234567890" />
-                  <Contact icon="ri-mail-unread-line" header="Email" content="info@example.com" />
-              </div>
-            </form>
+        <div className='h-[850px] lg:h-[700px] -mt-[60px] lg:-mt-[250px] rounded-xl mx-[1rem] lg:min-w-[420px] flex flex-col py-8 md:py-12 px-4 items-start shadow-lg border-lg bg-white'>
+          <h2 className="text-text_primary font-poppins font-semibold text-center flex items-center select relative pb-4  justify-center w-full text-[1.4em]">Select A Service! </h2>
+          <div className="py-6 w-full">
+            {
+              services.slice(1).map(service => (
+                <div key={service.id} className="w-full py-3">
+                  <RadioButton
+                    checked={selected === service.type}
+                    value={service.type}
+                    onChange={handleChange}
+                    name={service.name}
+                  />
+                </div>
+              ))
+            }
+          </div>
+          <Button text="Book Now" />
+          <div className="flex flex-col md:flex-row py-4 md:items-center w-full justify-between mt-5">
+            <Contact icon="ri-phone-line" header="Hotline" content="+11234567890" />
+            <Contact icon="ri-mail-unread-line" header="Email" content="info@example.com" />
+          </div>
         </div>
       </div>
     </div>
